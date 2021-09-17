15b705fce4a309386011ff41abd9a25683b5e5c2
MrOkun <74727162+MrOkun@users.noreply.github.com>
2021-09-17 20:39:59 +0300

    Добавьте файлы проекта.

---
+++ BottleSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static private int[] nums;
+
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+
+            nums = new int[10];
+            
+            for (int i = 0; i < nums.Length; i++)//ввод
+            {
+                nums[i] = rnd.Next(0, 100);
+            }
+
+            for (int t = 0; t < nums.Length; t++)//сортировка
+            {
+                for (int j = nums.Length - 1; j > t; j--)
+                {
+                    if (nums[j - 1] > nums[j])
+                    {
+                        var temp = nums[j - 1];
+                        var temp1 = nums[j];
+
+                        nums[j - 1] = temp1;
+                        nums[j] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)//вывод
+            {
+                Console.Write(nums[i] + " ");
+            }
+            
+        }
+    }
+}
